f819eb9691e99cfbfe25ab7cbb1a10d948048a46
demon2749 <demon2749@gmail.com>
2019-04-06 15:36:35 -0500

    adding my current progress.

---
+++ FourInARow/FourInARow/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FourInARow
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Set_activePlayer("Red");
+            Table myBoard = new Table(board);
+        }
+
+        Image currentPlayer;
+
+        private string Get_activePlayer()
+        {
+            return activePlayer.Text;
+        }
+
+        private void Set_activePlayer(string newActivePlayer)
+        {
+            if (newActivePlayer == "Red")
+            {
+                currentPlayer = FourInARow.Properties.Resources.Red;
+                pictureBox43.Image = currentPlayer;
+                pictureBox43.Refresh();
+            }
+            else if (newActivePlayer == "Blue")
+            {
+                currentPlayer = FourInARow.Properties.Resources.Blue;
+                pictureBox43.Image = currentPlayer;
+                pictureBox43.Refresh();
+            }
+            activePlayer.Text = newActivePlayer;
+        }
+
+        private void AnyPlayerDrop()
+        {
+            if (Get_activePlayer() == "Red")
+            {
+                Set_activePlayer("Blue");
+            }
+            else if (Get_activePlayer() == "Blue")
+            {
+                Set_activePlayer("Red");
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AnyPlayerDrop();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            AnyPlayerDrop();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            AnyPlayerDrop();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            AnyPlayerDrop();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            AnyPlayerDrop();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            AnyPlayerDrop();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            AnyPlayerDrop();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
---
+++ FourInARow/FourInARow/Table.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FourInARow
+{
+    class Table
+    {
+        public Table(TableLayoutPanel aTable)
+        {
+            myTable = aTable;
+        }
+
+        public TableLayoutPanel myTable;
+        public int[,] myBoard = new int[6, 5];
+        public int[] columnWeight = new int[6];
+
+        public void UpdateTable(int column, string player)
+        {
+            if (columnWeight[column] < 5)
+            {
+                columnWeight[column]++;
+                if (player == "Red")
+                {
+                    myBoard[column, columnWeight[column]] = 1;
+                }
+                else
+                {
+                    myBoard[column, columnWeight[column]] = 2;
+                }
+                UpdateImage(column, columnWeight[column]);
+                CheckForWin(player);
+            }
+        }
+
+        private void CheckForWin(string player)
+        {
+
+        }
+
+        private void UpdateImage(int column, int row)
+        {
+
+        }
+    }
+}
+
